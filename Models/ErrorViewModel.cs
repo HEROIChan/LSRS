53bc46df545ad3cf9657985655a9b6f96b88f6b7
ccz <1832083808@qq.com>
2020-12-07 14:56:36 +0800

    make the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LSRS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
