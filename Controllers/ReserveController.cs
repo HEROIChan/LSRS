11c37ff358724d4edd4da89726c97a2860efb736
ccz <1832083808@qq.com>
2020-12-07 16:49:53 +0800

    modified by ccz

---
+++ Controllers/ReserveController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace LSRS.Controllers
+{
+    public class  ReserveController : Controller
+    {
+        //get: /Reserve/
+        public string Index()
+        {
+            return "my default action";
+        }
+           
+    }
+        
+}
